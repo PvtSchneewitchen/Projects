f175b4b2304d1a222031f5144ae6ba36fa49104c
PvtSchneewitchen <patrick.gmm@web.de>
2017-11-10 14:33:15 +0100

    holoconection

--- capacitivesensor/SensorDataViewer/src/de/isse/robotics/SensorViewer.java
+++ capacitivesensor/SensorDataViewer/src/de/isse/robotics/SensorViewer.java
@@ -8,29 +8,29 @@ import java.net.InetAddress;
 public class SensorViewer {
 	private static String Host = "192.168.178.29";
 	private static int Port = 6;
-	
+
 
 	public static void main(String[] args) throws Exception {
 		HololensConnection hl = new HololensConnection();
 		InetAddress host = InetAddress.getByName(Host);
 		DatagramSocket serverSocket = new DatagramSocket(Port,host);
-		
-		if(hl.Connect(serverSocket)) {
-		SensorModel model = new SensorModel();
-		SensorViewerWindow svwInstance = new SensorViewerWindow(model);
-		svwInstance.setVisible(true);
 
-		ViconAccess viconClass = new ViconAccess();
-		viconClass.startVicon();
-		viconClass.startLogger();
-		
-		while(true) {
-			viconClass.processCapacityWithVicon(svwInstance.getMean());
-			viconClass.computeCapacitiesMean(svwInstance.getMean());
-			hl.sendCapacity1ToHololens(serverSocket, svwInstance.getMean()[0]);
-			hl.sendCapacity2ToHololens(serverSocket, svwInstance.getMean()[1]);
-			hl.sendDistanceToHololens(serverSocket, viconClass.getDistance());
+		if(hl.Connect(serverSocket)) {
+			SensorModel model = new SensorModel();
+			SensorViewerWindow svwInstance = new SensorViewerWindow(model);
+			svwInstance.setVisible(true);
+
+			ViconAccess viconClass = new ViconAccess();
+			viconClass.startVicon();
+			viconClass.startLogger();
+
+			while(true) {
+				viconClass.processCapacityWithVicon(svwInstance.getMean());
+				viconClass.computeCapacitiesMean(svwInstance.getMean());
+				hl.sendCapacity1ToHololens(serverSocket, svwInstance.getMean()[0]);
+				hl.sendCapacity2ToHololens(serverSocket, svwInstance.getMean()[1]);
+				hl.sendDistanceToHololens(serverSocket, viconClass.getDistance());
+			}
 		}
-	}
 	}
 }
